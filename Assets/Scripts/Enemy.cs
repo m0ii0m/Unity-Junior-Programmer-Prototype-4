ae784c57bdff277e1ac39f7f4004058139b38c02
m0ii0m <ludovic.regnier.2004@gmail.com>
2025-04-02 14:16:20 +0200

    Lesson 4.4 - For-Loops For Waves

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -19,6 +19,11 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(transform.position.y < -10)
+        {
+            Destroy(gameObject);
+        }
+
         Vector3 lookDirection = (player.transform.position - transform.position).normalized;
         enemyRb.AddForce(lookDirection * speed);
     }
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,19 +5,30 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
+    public int enemyCount;
+    public int waveNumber;
+    public GameObject powerupPrefab;
 
     private float spawnRange = 9;
 
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        waveNumber = 1;
+        SpawnEnemyWaves(waveNumber);
+        SpawnPowerup();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+        if (enemyCount == 0)
+        {
+            waveNumber++;
+            SpawnEnemyWaves(waveNumber);
+            SpawnPowerup();
+        }
     }
 
     private Vector3 GenerateSpawnPosition()
@@ -26,5 +37,25 @@ public class SpawnManager : MonoBehaviour
         float spawnPosZ = Random.Range(-spawnRange, spawnRange);
         Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
         return randomPos;
+    }
+
+    private void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+    }
+
+    private void SpawnPowerup()
+    {
+        Vector3 pos = GenerateSpawnPosition();
+        pos.y = -0.1f;
+        Instantiate(powerupPrefab, pos, powerupPrefab.transform.rotation);
+    }
+
+    private void SpawnEnemyWaves(int enemiesToSpawn)
+    {
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            SpawnEnemy();
+        }
     }
 }
